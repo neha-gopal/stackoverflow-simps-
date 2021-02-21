bb838f9798c4cc9d7497873886945acc4eff3752
neha-gopal <78199592+neha-gopal@users.noreply.github.com>
2021-02-21 06:55:50 -0800

    Add files via upload

---
+++ keyboard.py
@@ -0,0 +1,64 @@
+import keyboard
+
+x=0
+
+
+keystrokes = input("Start typing \n")
+while x==0:
+  if keystrokes == "fj":
+    print("a")
+  elif keystrokes == "fk":
+    print ("b")
+  elif keystrokes == "fl":
+    print ("c")
+  elif keystrokes == "f;":
+    print ("d")
+  elif keystrokes == "dj":
+    print ("e")
+  elif keystrokes == "dk":
+    print ("f")
+  elif keystrokes == "dl":
+    print ("g")
+
+  elif keystrokes == "d;":
+    print("h")
+  elif keystrokes == "sj":
+    print ("i")
+  elif keystrokes == "sk":
+    print ("j")
+  elif keystrokes == "sl":
+    print ("k")
+  elif keystrokes == "s;":
+    print("l")
+  elif keystrokes == "aj":
+    print("m")
+
+  elif keystrokes == "ak":
+    print ("n")
+  elif keystrokes == "al":
+    print ("o")
+  elif keystrokes == "a;":
+    print ("p")
+  elif keystrokes == "jk":
+   print ("q")
+  elif keystrokes == "jl":
+   print ("r")
+  elif keystrokes == "j;":
+   print ("s")
+
+
+  elif keystrokes == "fd":
+    print ("t")
+  elif keystrokes == "fs":
+    print ("u")
+  elif keystrokes == "fa":
+    print ("v")
+  elif keystrokes == "aa":
+    print ("w")
+  elif keystrokes == "ss":
+    print ("x")
+  elif keystrokes == "dd":
+    print ("y")
+  elif keystrokes == "ff":
+    print ("z")
+  keystrokes = input("")
